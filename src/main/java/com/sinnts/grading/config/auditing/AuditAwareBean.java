21adb8f0a5c31fb32b186810c7b6599193eead65
Muhammad Yunus Abu-Dukhan <66983235+Dhannun@users.noreply.github.com>
2024-07-05 09:53:11 +0100

    ✨ Artifactory
    Added Nexus Releases and Snapshots repositories for artifactory

--- src/main/java/com/sinnts/grading/config/auditing/AuditAwareBean.java
+++ src/main/java/com/sinnts/grading/config/auditing/AuditAwareBean.java
@@ -2,8 +2,10 @@ package com.sinnts.grading.config.auditing;
 
 import com.sinnts.grading.user.User;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.data.domain.AuditorAware;
 
+@Configuration
 public class AuditAwareBean {
   @Bean
   public AuditorAware<User> auditorAware() {
